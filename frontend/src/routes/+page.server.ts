c45c7bdb4d41fe40cf94188fe2f13dfdbefb62f1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-01-01 12:27:03 +0530

    Using Environment variable for backend API

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,12 +1,13 @@
-import { redirect } from '@sveltejs/kit';
-import { validateSessionToken } from '$lib/server/session';
+import { redirect } from "@sveltejs/kit";
+import { validateSessionToken } from "$lib/server/session";
+import { env } from "$env/dynamic/private";
 
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
 	// Check for session cookies
-	if (event.cookies.get('session')) {
-		const sessionToken = event.cookies.get('session')!;
+	if (event.cookies.get("session")) {
+		const sessionToken = event.cookies.get("session")!;
 
 		// If it exists, validate the session token
 		const session = await validateSessionToken(sessionToken);
@@ -14,19 +15,19 @@ export const load: PageServerLoad = async (event) => {
 		// If the session does not exist
 		// redirect to login page
 		if (!session) {
-			return redirect(302, '/login');
+			return redirect(302, "/login");
 		}
 
 		// API Hit Example
-		const response = await fetch('http://localhost:8080/dummy', {
+		const response = await fetch(`${env.PHOTOATOM_BACKEND_API_URL}/dummy`, {
 			headers: {
-				Authorization: `Bearer ${session?.accessToken}`
-			}
+				Authorization: `Bearer ${session?.accessToken}`,
+			},
 		});
 
 		console.log(await response.text());
 		console.log(response.status);
 	} else {
-		return redirect(302, '/login');
+		return redirect(302, "/login");
 	}
 };
