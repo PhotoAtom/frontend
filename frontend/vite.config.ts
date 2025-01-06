66d49058ba9264c3f2cd675ac5f6698afd5310c1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-01-06 11:46:34 +0530

    TLS Configuration for PhotoAtom Frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(async ({ mode }) => {
+
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			https: {
+				cert: await Bun.file(env.TLS_CERT).text(),
+				key: await Bun.file(env.TLS_CERT_KEY).text(),
+			},
+			proxy: {}
+		}
+	};
 });
